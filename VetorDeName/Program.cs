5db5368db0a0b6c3014a37e34fc0bc6c65001a55
Everton Silva <everton.tps@outlook.com>
2025-10-09 16:40:58 -0300

    Adicionar arquivos de projeto.

---
+++ VetorDeName/Program.cs
@@ -0,0 +1,68 @@
+﻿int i = 0, tamanho = 0;
+
+Console.Write("Informe o tamanho do vetor: ");
+tamanho = int.Parse(Console.ReadLine());              // coleta de um número inteiro pelo usuário
+
+Console.WriteLine(); // Pular linha
+
+
+int[] numeros = new int[tamanho]; // Criação do vetor 'numeros' recebendo como qtdd a leitura do usuário (variável>tamanho)
+
+
+//  <<< [1] >>>
+//--------------------------------------------------------------------------------------------------------------------------------------
+while (i < tamanho)
+{
+    numeros[i] = Random.Shared.Next(1, 100);  //Gerar números aleátorios de acordo com a qtdd informada pelo usuário, sendo do intervalo 1 a 100
+    i++;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+//  <<< [2] >>>
+//--------------------------------------------------------------------------------------------------------------------------------------
+Console.WriteLine("Vetor inteiro");
+for (i = 0; i < numeros.Length; i++)
+{
+    Console.Write(numeros[i] + " ");  // Impressão do vetor inteiro
+}
+
+Console.WriteLine("\n\n"); // Pular linha
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+//  <<< [3] >>>
+//--------------------------------------------------------------------------------------------------------------------------------------
+Console.WriteLine("Primeira metade");
+for  (i = 0; i < (numeros.Length/2); i++)
+{
+    Console.Write(numeros[i] + " ");  // Impressão da primeira metade do vetor
+}
+
+Console.WriteLine("\n\n"); // Pular linha
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+//  <<< [4] >>>
+//--------------------------------------------------------------------------------------------------------------------------------------
+Console.WriteLine("Segunda metade");
+for (i = (numeros.Length/2); i < numeros.Length; i++)
+{
+    Console.Write(numeros[i] + " ");    // Impressão da segunda metade do vetor
+}
+
+Console.WriteLine("\n\n"); // Pular linha
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+//  <<< [5] >>>
+//--------------------------------------------------------------------------------------------------------------------------------------
+Console.WriteLine("Segunda metade ao contrário");
+for (i = numeros.Length -1; (i >= numeros.Length/2); i--)
+{
+    Console.Write(numeros[i] + " ");    // Impressão da segunda metade do vetor ao contrário
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+Console.ReadKey();
---
+++ Vetores/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numeros = new int [5];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = Random.Shared.Next(1, 100);
+}
+
+int contador = 0;
+
+do
+{
+    Console.WriteLine(numeros[contador]);
+    contador++;
+} while (contador < numeros.Length);
