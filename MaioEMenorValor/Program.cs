0dc5f7d5d7eb6bc87ad3beffb101cc48e5397ef1
Everton Silva <everton.tps@outlook.com>
2025-10-09 19:08:30 -0300

    Alguns exercicios resolvidos

---
+++ MaioEMenorValor/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] numeros = new double[10];
+double maior = 0;
+
+Console.WriteLine("Digite 10 números:\n");
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write($"Número {i+1}: ");
+    numeros[i] = double.Parse(Console.ReadLine());
+    if (maior < numeros[i])
+        maior = numeros[i];
+}
+Console.WriteLine($"\nO maior número é {maior}");
---
+++ SomaDeElementos/Program.cs
@@ -0,0 +1,13 @@
+﻿int soma = 0;
+int[] numeros = new int[5];
+
+Console.WriteLine("Digite 5 números inteiros pra soma: ");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Número{i+1}: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+    soma += numeros[i];
+}
+
+Console.WriteLine($"A soma dos números é: {soma}");
